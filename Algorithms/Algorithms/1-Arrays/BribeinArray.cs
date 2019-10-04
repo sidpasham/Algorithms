c5b8018f6161b97e3ec7e80a9eaa122b0f789895
Sid Pasham <spasham@archcapservices.com>
2019-10-04 16:56:47 -0400

    removed leetcode problems and put in different solution

--- Algorithms/Algorithms/1-Arrays/BribeinArray.cs
+++ Algorithms/Algorithms/1-Arrays/BribeinArray.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Algorithms.HackerRank
+namespace Algorithms
 {
     class BribeinArray
     {
--- Algorithms/Algorithms/2-Strings/LongestCommonPrefix.cs
+++ Algorithms/Algorithms/2-Strings/LongestCommonPrefix.cs
@@ -1,4 +1,6 @@
-﻿namespace Algorithms.LeetCode
+﻿using System;
+
+namespace Algorithms
 {
     class LongestCommonPrefix
     {
@@ -9,15 +11,17 @@
         //    Console.WriteLine(lcp);
         //}
 
+        //O(n^2) time complexity
+        //O(1) space
         static string GetLongestCommonPrefix(string[] str)
         {
             if (str.Length == 0) return "";
 
             var prefix = str[0];
 
-            for (int i = 0; i < str.Length; i++)
+            for(int i = 0; i < str.Length; i++)
             {
-                while (str[i].IndexOf(prefix) != 0)
+                while (str[i].IndexOf(prefix, StringComparison.Ordinal) != 0)
                 {
                     prefix = prefix.Substring(0, prefix.Length - 1);
                 }
