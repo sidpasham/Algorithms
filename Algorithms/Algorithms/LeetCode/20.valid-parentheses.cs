5f01b5ad663ce4f7f7d12b74459ecdead1e2abc6
Sid Pasham <spasham@archcapservices.com>
2019-10-03 23:31:01 -0400

    add leet code problems

---
+++ Algorithms/Algorithms/LeetCode/20.valid-parentheses.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=20 lang=csharp
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+public class Solution {
+    public bool IsValid(string s) {
+        
+    }
+}
+// @lc code=end
+
